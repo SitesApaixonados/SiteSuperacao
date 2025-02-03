e02535a0edc087723e8eb0f74c01eb4260a63a45
Aliragm <andrelira50@gmail.com>
2025-02-03 04:07:21 -0300

    projeto leitao

---
+++ script.js
@@ -0,0 +1,22 @@
+function tempoPassado(data) {
+    const agora = new Date();
+    const diferenca = agora - data;
+
+    const segundos = Math.floor(diferenca / 1000) % 60;
+    const minutos = Math.floor(diferenca / (1000 * 60)) % 60;
+    const horas = Math.floor(diferenca / (1000 * 60 * 60)) % 24;
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+
+    return `${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`;
+}
+
+const data = new Date(2025, 0, 23, 20, 1, 39);
+console.log(tempoPassado(data));
+
+function atualizarTempo() {
+    document.getElementById("tempo").textContent = tempoPassado(data);
+}
+
+setInterval(atualizarTempo, 1000);
+
+atualizarTempo();
